7995f8d26126f934e2f9171dfc2afac38a8248e9
RolandoZhao <45811113+RolandoZhao@users.noreply.github.com>
2025-12-29 17:11:33 +0800

    Update branding.ts

--- packages/const/src/branding.ts
+++ packages/const/src/branding.ts
@@ -1,13 +1,13 @@
 // the code below can only be modified with commercial license
 // if you want to use it in the commercial usage
-// please contact us for more information: hello@lobehub.com
+// please contact us for more information: hello@万福AI.com
 
-export const LOBE_CHAT_CLOUD = 'LobeHub Cloud';
+export const LOBE_CHAT_CLOUD = '万福AI Cloud';
 
-export const BRANDING_NAME = 'LobeHub';
+export const BRANDING_NAME = '万福AI';
 export const BRANDING_LOGO_URL = '';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = '万福AI';
 
 export const BRANDING_URL = {
   help: undefined,
@@ -17,13 +17,13 @@ export const BRANDING_URL = {
 
 export const SOCIAL_URL = {
   discord: 'https://discord.gg/AYFPHvv2jT',
-  github: 'https://github.com/lobehub',
-  medium: 'https://medium.com/@lobehub',
-  x: 'https://x.com/lobehub',
-  youtube: 'https://www.youtube.com/@lobehub',
+  github: 'https://github.com/万福AI',
+  medium: 'https://medium.com/@万福AI',
+  x: 'https://x.com/万福AI',
+  youtube: 'https://www.youtube.com/@万福AI',
 };
 
 export const BRANDING_EMAIL = {
-  business: 'hello@lobehub.com',
-  support: 'support@lobehub.com',
+  business: 'hello@万福AI.com',
+  support: 'support@万福AI.com',
 };
